7b03b28615bd7a15055fe3e8162e886d75d0ebb2
moratadave <davidopara2019@gmail.com>
2022-03-18 17:04:39 +0100

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,9 @@
 <?php
 echo "</br>Copyright &copy 1999 - ". date("Y") . ' DAVID TECH.'."<br>";
 ?>
-<button type="button" class="btn btn-success">Submit here</button>
+<button type="button" class="btn btn-success">Submit here</button><br>
+<button type="button" class="btn btn-success">Save</button>
+
 </div>
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
